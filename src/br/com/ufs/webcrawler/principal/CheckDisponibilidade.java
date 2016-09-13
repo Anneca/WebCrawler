c3c75de618439dc4ab2edaa7f7b3a5425551ddb5
Anneca <anneca1992@gmail.com>
2016-09-13 11:29:00 -0300

    Verificando disponibilidade do site

---
+++ src/br/com/ufs/webcrawler/principal/CheckDisponibilidade.java
@@ -0,0 +1,55 @@
+package br.com.ufs.webcrawler.principal;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import br.com.ufs.webcrawler.model.Formulario;
+
+/**
+ * 
+ * @author Anne Caroline Melo Santos
+ *
+ */
+public class CheckDisponibilidade {
+
+	Document doc;
+	boolean disponibilidade;
+
+	public boolean verificarDisponibilidadeSite(String url) {
+
+		disponibilidade = false;
+
+		try {
+			// https://foradoar.org/ site utilizado para verificar
+			// disponibilidade do site
+			doc = Jsoup.connect("http://downforeveryoneorjustme.com/" + url)
+					.userAgent(
+							"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
+					.get();
+			// Capturando os elementos
+			Elements resultLinks = doc.select("div");
+
+			for (Element element : resultLinks) {
+				
+				if (element.text().matches(".*(?i)looks down from here.*") && !element.text().equals("")) {
+					System.out.println("Site fora do AR");
+				}
+
+				if (element.text().matches(".*(?i)is up.*") && !element.text().equals("")) {
+					System.out.println("Este site está no ar");
+					disponibilidade = true;
+
+				}
+
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return disponibilidade;
+
+	}
+}
--- src/br/com/ufs/webcrawler/principal/Main.java
+++ src/br/com/ufs/webcrawler/principal/Main.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 import br.com.ufs.webcrawler.dao.HospitalDAO;
 import br.com.ufs.webcrawler.model.Formulario;
 import br.com.ufs.webcrawler.model.Hospital;
+
 /**
- * Universidade Federal de Sergipe
- * Trabalho de Conclusão de Curso II
- * Projeto WebCrawler - Observatória e-Health
+ * Universidade Federal de Sergipe Trabalho de Conclusão de Curso II Projeto
+ * WebCrawler - Observatória e-Health
+ * 
  * @author Anne Caroline Melo Santos
  *
  */
@@ -21,22 +22,32 @@ public class Main {
 		ExtratorTecnologias extratorTecnologias = new ExtratorTecnologias();
 		ExtratorInformacoesHospital extratorInformacoesHospital = new ExtratorInformacoesHospital();
 		HospitalDAO hospitalDAO = new HospitalDAO();
+		CheckDisponibilidade verificador = new CheckDisponibilidade();
+		boolean disponivel;
+		String url;
 
 		ArrayList<Hospital> hospitais = new ArrayList<Hospital>();
 
 		try {
 			hospitais = hospitalDAO.listarHospitais();
+
+			for (int i = 0; i < hospitais.size(); i++) {
+				url = hospitais.get(i).getUrl();
+				disponivel = verificador.verificarDisponibilidadeSite(url.replaceAll("http://", ""));
+				if (disponivel) {
+					Formulario formulario = extratorInformacoesHospital.extrairInformacoesHospital(url);
+					System.out.println("Tecnologias:");
+					extratorTecnologias.extrairTecnologias(url.replaceAll("http://", ""), formulario);
+					System.out.println("Redes Sociais:");
+					extratorRedesSociais.extrairRedesSociais(url, formulario);
+				}
+				else {
+					System.out.println("Tente outra hora amiguinho!!");
+				}
+			}
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-
-		for (int i = 0; i < hospitais.size(); i++) {
-			String url = hospitais.get(i).getUrl();
-			Formulario formulario = extratorInformacoesHospital.extrairInformacoesHospital(url);
-			System.out.println("Tecnologias:");
-			extratorTecnologias.extrairTecnologias(url.replaceAll("http://", ""), formulario);
-			System.out.println("Redes Sociais:");
-			extratorRedesSociais.extrairRedesSociais(url, formulario);
 		}
 	}
 
